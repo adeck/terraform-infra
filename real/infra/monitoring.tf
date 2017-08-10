01d6e4a5f42c27044d69d86a4779baebbb5311fc
Andrew Deck <adeck@adobe.com>
2017-08-09 19:15:42 -0700

    changed icinga to monitoring

--- real/infra/monitoring.tf
+++ real/infra/monitoring.tf
@@ -1,12 +1,12 @@
 
-module "appliance_icinga" {
-    name = "icinga"
+module "appliance_monitoring" {
+    name = "monitoring"
     source = "./appliance"
     vpc_name = "${ var.vpc_name }"
     key_name = "${ aws_key_pair.main.key_name }"
     security_group_ids = [
         "${ aws_security_group.main.id }",
-        "${ aws_security_group.icinga.id }"
+        "${ aws_security_group.monitoring.id }"
     ]
     instance_ami = "${ data.aws_ami.main.id }"
     subnet_id = "${ aws_subnet.infra.id }"
@@ -14,9 +14,9 @@ module "appliance_icinga" {
     private_domain_name = "${ aws_route53_zone.private.name }"
 }
 
-resource "aws_security_group" "icinga" {
-  name        = "${ var.vpc_name }-icinga"
-  description = "icinga master servers"
+resource "aws_security_group" "monitoring" {
+  name        = "${ var.vpc_name }-monitoring"
+  description = "monitoring master servers"
   vpc_id = "${ aws_vpc.main.id }"
 
   ingress {
@@ -27,7 +27,7 @@ resource "aws_security_group" "icinga" {
   }
 
   tags {
-      Name = "${ var.vpc_name }-icinga"
+      Name = "${ var.vpc_name }-monitoring"
   }
 }
 
