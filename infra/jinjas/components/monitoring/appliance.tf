d8c565547f0c34e0a606fa55a36fb3653194c0b8
Andrew Deck <adeck@adobe.com>
2017-08-24 21:28:35 -0700

    added iam profile for grafana

--- infra/jinjas/components/monitoring/appliance.tf
+++ infra/jinjas/components/monitoring/appliance.tf
@@ -1,6 +1,8 @@
 
 {% import "macros/appliance.tf" as m with context %}
 
-{{ m.appliance('monitoring-infra', subnet="infra", security_group="monitoring") }}
+{{ m.appliance('monitoring-infra', subnet="infra",
+        security_group="monitoring",
+        iam_profile="${ aws_iam_instance_profile.monitoring.name }") }}
 
 
