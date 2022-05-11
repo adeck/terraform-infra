441306ac194cbe7b92ab11231941886a4a8da3c3
garin <nope@nope>
2022-05-11 14:14:24 -0700

    updating syntax for appliance code...

--- real/infra/appliance/main.tf
+++ real/infra/appliance/main.tf
@@ -1,30 +1,34 @@
 
 resource "aws_route53_record" "private" {
-  zone_id = "${ var.private_zone_id }"
-  name    = "${ var.name }-infra"
+  zone_id = var.private_zone_id
+  name    = var.name
   type    = "A"
   ttl     = "60"
-  records = ["${ aws_instance.main.private_ip }"]
+  records = [aws_instance.main.private_ip]
 }
 
 resource "aws_instance" "main" {
-  ami           = "${ var.instance_ami }"
-  instance_type = "t2.micro"
-  key_name = "${ var.key_name }"
-  associate_public_ip_address = false # making it explicit
-  vpc_security_group_ids = ["${ var.security_group_ids }"]
-  subnet_id = "${ var.subnet_id }"
-  user_data = "${ data.template_file.main.rendered }"
-  tags {
-    Name = "${ var.vpc_name }-${ var.name }"
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+  vpc_security_group_ids = var.security_group_ids
+  subnet_id = var.subnet_id
+  user_data = data.template_file.main.rendered
+  root_block_device {
+    volume_size = var.volume_size
+  }
+  tags = {
+    Name = "${ var.name }-${ var.vpc_name }"
     Description = "Managed by terraform"
   }
+  # Making this explicit actually introduces a bug, because by default no public IP is assigned, but for a public endpoint this will *become* true, so every subsequent apply run will recreate all public instances.
+  # associate_public_ip_address = false # making it explicit
 }
 
 data "template_file" "main" {
-    template = "${ file("${path.module}/cloudinit.yml") }"
-    vars {
-        hostname = "${ var.name }-infra.${ var.private_domain_name }"
+    template = "${ file("${path.module}/cloudinit.yaml") }"
+    vars = {
+        hostname = "${ var.name }.${ var.private_domain_name }"
     }
 }
 
--- real/infra/appliance/variables.tf
+++ real/infra/appliance/variables.tf
@@ -1,21 +1,32 @@
 
 ### inputs
 
-variable "name" {}
+variable "name" {type = string}
 
-variable "instance_ami" {}
-
-variable "vpc_name" {}
-variable "key_name" {}
-variable "security_group_ids" {type = "list"}
-variable "subnet_id" {}
-variable "private_zone_id" {}
-variable "private_domain_name" {}
+variable "instance_ami" {type = string}
+variable "instance_type" {
+    type = string
+    # even this might be more than we need
+    # as of this writing it's 0.0104 / hr in oregon and a couple other regions
+    # so, average of $7.5972 / month / appliance
+    default = "t3.micro"
+}
+# value in GiB, as in aws_instance
+variable "volume_size" {
+    type = number
+    default = 8
+}
+variable "vpc_name" {type = string}
+variable "key_name" {type = string}
+variable "security_group_ids" {type = list(string)}
+variable "subnet_id" {type = string}
+variable "private_zone_id" {type = string}
+variable "private_domain_name" {type = string}
 
 ### outputs
 
 output "instance_id" {
-    value = "${ aws_instance.main.id }"
+    value = aws_instance.main.id
 }
 
 
