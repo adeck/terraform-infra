a04f1bff751301ed6978044de7a8cd0c03bc3419
garin <nope@nope>
2022-05-11 14:09:03 -0700

    more of those old changes

---
+++ real/infra/dns.tf
@@ -0,0 +1,38 @@
+
+data "aws_route53_zone" "main" {
+    name = "${ var.domain }"
+}
+
+resource "aws_route53_zone" "infra" {
+    name = "infra.${ var.domain }"
+}
+
+
+resource "aws_route53_record" "infra-ns" {
+  zone_id = "${ data.aws_route53_zone.main.zone_id }"
+  name    = "${ aws_route53_zone.infra.name }"
+  type    = "NS"
+  ttl     = "60"
+
+  records = ["${ aws_route53_zone.infra.name_servers }"]
+}
+
+# for internal / private DNS resolution
+resource "aws_route53_zone" "private" {
+    name = "local"
+    vpc {
+        vpc_id = "${ aws_vpc.main.id }"
+    }
+}
+
+resource "aws_vpc_dhcp_options" "main" {
+    domain_name = "local"
+    domain_name_servers = ["169.254.169.253"]
+}
+
+resource "aws_vpc_dhcp_options_association" "main" {
+  vpc_id          = "${ aws_vpc.main.id }"
+  dhcp_options_id = "${ aws_vpc_dhcp_options.main.id }"
+}
+
+
---
+++ real/infra/main.tf
@@ -0,0 +1,64 @@
+
+provider "aws" {
+  access_key = "${ var.access_key }"
+  secret_key = "${ var.secret_key }"
+  region     = "${ var.geo }"
+}
+
+data "aws_ami" "main" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-stretch-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["379101102735"] # Debian
+}
+
+resource "aws_key_pair" "main" {
+    key_name   = "${ var.vpc_name }-main"
+    public_key = "${ var.public_key }"
+}
+
+resource "aws_security_group" "main" {
+  name        = "${ var.vpc_name }-main"
+  description = "Default rules for hosts in ${ var.vpc_name } VPC"
+  vpc_id = "${ aws_vpc.main.id }"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol = "tcp"
+    security_groups = ["${ aws_security_group.gw.id }"]
+  }
+
+#  ingress {
+#    from_port   = 5666
+#    to_port     = 5666
+#    protocol = "tcp"
+#    security_groups = ["${ aws_security_group.icinga.id }"]
+#  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags {
+      Name = "${ var.vpc_name }-main"
+  }
+}
+
