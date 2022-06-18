9a7e07236e46c8f4f7b74b150fbbb8d3e8e693cd
garin <nope@nope>
2022-06-18 14:07:01 -0700

    removed all the unrelated jinja2 stuff

--- scripts/jinjas/components/gw/main.tf
+++
@@ -1,6 +0,0 @@
-
-{% import "macros/appliance.tf" as m with context %}
-
-{{ m.appliance('gw-infra', subnet="dmz", security_group="gw") }}
-
-
--- scripts/jinjas/components/salt/appliance.tf
+++
@@ -1,6 +0,0 @@
-
-{% import "macros/appliance.tf" as m with context %}
-
-{{ m.appliance('salt-infra', subnet="infra", security_group="salt") }}
-
-
